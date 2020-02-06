087c34c99cdc1a95fe328e0db6af69c1a51ce26d
Gulot13 <thejusok@gmail.com>
2020-02-06 11:58:27 +0100

    first commit

---
+++ app/Title.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Title extends Model
+{
+    //
+    protected $primaryKey = "emp_no";
+    public $timestamps = false;
+
+    public function employee() {
+        return $this->belongsTo('App\Employee', 'emp_no', 'emp_no');
+    }
+}
